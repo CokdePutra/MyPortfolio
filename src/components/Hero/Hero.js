41f8338a4d5580b6f46f81cc045537047e4ed125
CokdePutra <cokgdeputrawidnyana25@gmail.com>
2023-04-17 15:11:18 +0800

    My Portfolio

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = (props) => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        Cokde Putra Portfolio
+      </SectionTitle>
+      <SectionText>
+        JavaScript is a programming language that is primarily used for creating
+        interactive web pages and web applications.
+      </SectionText>
+      <Button onClick={() => (window.location = "#")}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+export default Hero;
---
+++ src/constants/constants.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: "School Tuition Payment",
+    description:
+      "Using HTML, CSS, JavaScript, PHP. This Website made for a school admin to insert Payment Tuition for Students and Students also can check their data, like how many they already paid the Tuition.",
+    image: "/images/PaymentSPP.png",
+    tags: ["HTML", "CSS", "JavaScript", "PHP"],
+    source: "https://github.com/CokdePutra/pembayaran-spp",
+    visit: "https://github.com/CokdePutra/pembayaran-spp",
+    id: 0,
+  },
+  {
+    title: "Pharmacy Drug Transactions Website",
+    description:
+      "A pharmacy drug transactions website is an online platform that enables pharmacies, healthcare providers, and patients to conduct drug transactions electronically. These websites provide a convenient and secure way for people to purchase prescription drugs, over-the-counter medications, and other health-related products.",
+    image: "/images/Apotek.png",
+    tags: ["HTML", "Bootstrap", "JavaScript", "PHP"],
+    source: "https://github.com/CokdePutra/Web-apotek",
+    visit: "https://github.com/CokdePutra/Web-apotek",
+    id: 1,
+  },
+  {
+    title: "WebRTC App",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 2,
+  },
+  {
+    title: "Unichat",
+    description:
+      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
